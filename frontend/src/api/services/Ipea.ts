54ad05bc6ae204b26f5e7661236c45e1869553f7
Jailton Santos <jailtonmatos200@gmail.com>
2026-06-18 17:26:02 -0300

    feat: implementation of new endpoints in this service

--- frontend/src/api/services/Ipea.ts
+++ frontend/src/api/services/Ipea.ts
@@ -2,6 +2,10 @@ import { apiClient } from '../client/Client';
 import type { IpeaSerie } from '../../types/IpeaType';
 
 export const ipeaService = {
+
+   // ==========================================
+  // ECONOMIA EM GERAL
+  // ==========================================
   getMacro: () =>
     apiClient.get<IpeaSerie[]>('/ipea/macro').then((res) => res.data),
 
@@ -21,5 +25,109 @@ export const ipeaService = {
   getPopulacao: () =>
     apiClient.get<IpeaSerie[]>('/ipea/populacao').then((res) => res.data),
 
+  getIbovespaClosing: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/mercado/ibovespa').then((res) => res.data),
+
   
+
+  // ==========================================
+  // BALANÇA DE PAGAMENTOS
+  // ==========================================
+  getAtivosReservas: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/balanca/ativos-reservas').then((res) => res.data),
+
+  getTransacoesCorrentes: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/balanca/transacoes-correntes').then((res) => res.data),
+
+  getBalancaComercial: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/balanca/comercial').then((res) => res.data),
+
+  getServicosBalanca: () => 
+    apiClient.get<IpeaSerie[]>('/ipea/balanca/servicos').then((res) => res.data),
+
+  getRendaPrimaria: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/balanca/renda-primaria').then((res) => res.data),
+
+  getInvestimentoDireto: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/balanca/investimento-direto').then((res) => res.data),
+
+  getContaCapital: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/balanca/conta-capital').then((res) => res.data),
+
+  getContaFinanceira: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/balanca/conta-financeira').then((res) => res.data),
+
+  getInvestimentoCarteira: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/balanca/investimento-carteira').then((res) => res.data),
+
+  getServicosDespesas: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/balanca/servicos-despesas').then((res) => res.data),
+
+  getInvestimentoDiretoIngressos: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/balanca/investimento-direto-ingressos').then((res) => res.data),
+
+
+
+
+  // ==========================================
+  // EXPORTAÇÕES
+  // ==========================================
+  getTotalExports: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/exportacoes/total').then((res) => res.data),
+
+  getExportQuantumIndex: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/exportacoes/quantum').then((res) => res.data),
+
+  getBasicProductsExports: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/exportacoes/produtos-basicos').then((res) => res.data),
+
+  getAgricultureLivestockQuantumExports: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/exportacoes/quantum-agricultura-pecuaria').then((res) => res.data),
+
+  getConsumerGoodsExports: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/exportacoes/bens-consumo').then((res) => res.data),
+
+  getCapitalGoodsPriceIndex: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/exportacoes/precos-bens-capital').then((res) => res.data),
+
+  getDurableConsumerGoodsPriceIndex: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/exportacoes/precos-bens-duraveis').then((res) => res.data),
+
+  getNonDurableConsumerGoodsPriceIndex: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/exportacoes/precos-bens-nao-duraveis').then((res) => res.data),
+
+  getIntermediateGoodsFobValue: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/exportacoes/valor-bens-intermediarios').then((res) => res.data),
+
+  getIntermediateGoodsQuantumIndex: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/exportacoes/quantum-bens-intermediarios').then((res) => res.data),
+
+  getFuelsFobValue: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/exportacoes/valor-combustiveis').then((res) => res.data),
+
+
+
+  // ==========================================
+  // IMPOSTOS
+  // ==========================================
+  getImportTax: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/impostos/importacao').then((res) => res.data),
+
+  getPersonalIncomeTax: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/impostos/irpf').then((res) => res.data),
+
+  getCorporateIncomeTax: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/impostos/irpj').then((res) => res.data),
+
+  getTotalIncomeTax: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/impostos/ir-total').then((res) => res.data),
+
+  getIofTax: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/impostos/iof').then((res) => res.data),
+
+  getIpiTax: () =>
+    apiClient.get<IpeaSerie[]>('/ipea/impostos/ipi').then((res) => res.data),
 };
+
+
+
